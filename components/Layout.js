a3d58d63331fc27966d312e5fca70906ed3832c7
meanseo <chlalstjok97@gmail.com>
2022-04-28 14:27:52 +0900

    new branch

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,9 @@ import styles from '@/styles/Layout.module.css'
 import {Footer, Header, Nav, Table, Pagination, Modal} from "@/components";
 export function Layout({ children }){
   return (<div className={styles.container}>
-        <Header/>
-        <Nav/>
         <main className={styles.main}>{children}</main>
         <Table/>
         <Pagination/>
-        <Footer/>
         <Modal/>
       </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { wrapper } from '@/modules/store'
+import { Header, Nav, Footer } from '@/components'
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+    <Header/>
+    <Nav/>
+    <Component {...pageProps} />
+    <Footer/>
+    </> 
+  )
 }
-
-export default MyApp
+export default wrapper.withRedux(App) 
