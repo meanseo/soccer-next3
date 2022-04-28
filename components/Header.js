e2aa8aec2820b047e0a08a2fe3240293f660e246
meanseo <chlalstjok97@gmail.com>
2022-04-28 12:09:48 +0900

    first commit

---
+++ components/Header.js
@@ -0,0 +1,4 @@
+import styles from '@/styles/Header.module.css'
+export function Header(){
+    return(<></>)
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Layout } from '@/components'
+
+export default function Home() {
+  return (
+    <Layout/>
+  )
+}
